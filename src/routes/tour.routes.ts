765e0b5df917cccec911e902c2059647b3c1a3c8
Leonel Alejandro Ceballos Alvarado <lceballos5@ucol.mx>
2024-07-30 14:33:43 -0600

    add tours-endpoint

---
+++ src/routes/tour.routes.ts
@@ -0,0 +1,31 @@
+import { Request, Response, Router } from "express";
+import prisma from "../prisma";
+
+const router = Router();
+
+router.get('/tours', async(req: Request, res: Response) =>{
+    try {
+        const tours = await prisma.tour.findMany();
+        res.json(tours);
+    } catch (error) {
+        res.status(500).json({ error: 'Error fetching tours' });
+    }
+})
+
+router.get("/tour/:id", async(req: Request, res: Response) =>{
+    const { id } = req.params;
+    try {
+        const tour = await prisma.tour.findUnique({
+            where: { id },
+        });
+        if (tour) {
+            res.json(tour);
+          } else {
+            res.status(404).json({ error: 'Tour not found' });
+          }
+    } catch (error) {
+        res.status(500);
+    }
+})
+
+export default router;
