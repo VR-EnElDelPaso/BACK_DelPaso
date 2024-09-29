9e550442827f2ea80cc9f5621f6fb450ba6ca860
Branyoe <branyoe.dev@gmail.com>
2024-09-29 17:43:51 -0600

    feat: add tour-suggestion endpoint

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
+import prismaRandom from 'prisma-extension-random';
 
-const prisma = new PrismaClient();
+
+const prisma = new PrismaClient().$extends(prismaRandom());
 
 export default prisma;
