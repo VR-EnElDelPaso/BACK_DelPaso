52e6dbd1b432c6948f55a72a7a13bc2ac00abfbc
Branyoe <branyoe.dev@gmail.com>
2024-10-17 21:10:43 -0600

    chore: change MySQL to Postgres

--- prisma/migrations/20240719220214_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE `recorridos_comprados` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `token` VARCHAR(191) NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20240909154748_account_number_unique/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[account_number]` on the table `user` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX `user_account_number_key` ON `user`(`account_number`);
