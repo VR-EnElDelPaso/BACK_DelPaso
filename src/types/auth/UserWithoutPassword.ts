6afd69fe83203b97bce2aad5150d906ce54572ea
Branyoe <branyoe.dev@gmail.com>
2025-01-23 22:44:20 -0600

    refactor: update import statement for User type in UserWithoutPassword interface

--- src/types/auth/UserWithoutPassword.ts
+++ src/types/auth/UserWithoutPassword.ts
@@ -1,3 +1,3 @@
-import { user } from "@prisma/client";
+import { User } from "@prisma/client";
 
-export default interface UserWithoutPassword extends Omit<user, "password"> { }
+export default interface UserWithoutPassword extends Omit<User, "password"> { }
