d2152862aa9ee384500bee4ef23cbc39b1b507b3
Branyoe <branyoe.dev@gmail.com>
2025-06-09 20:25:36 -0600

    feat(accreditations): fetch only expired accreditations in controller

--- src/controllers/accreditations.controllers.ts
+++ src/controllers/accreditations.controllers.ts
@@ -3,12 +3,24 @@ import { ResponseData } from "../types/ResponseData";
 import prisma from "../prisma";
 
 export const getAllAccreditationsController: RequestHandler = async (req, res) => {
-  prisma.tourAccess.findMany({
+  const accreditations = await prisma.tourAccess.findMany({
     where: {
-      
+      OR: [
+        {
+          expires_at: {
+            lte: new Date(), // Fetch only expired accreditations
+          }
+        },
+        {
+          expired: true // Fetch only expired accreditations
+        }
+      ]
     }
+  })
+
   return res.status(200).json({
     ok: true,
     message: "Accreditations fetched successfully",
+    data: accreditations,
   } as ResponseData)
 }
