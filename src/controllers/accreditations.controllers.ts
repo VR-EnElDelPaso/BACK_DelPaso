1f6083e47cabc654571e103e44673e05fee37f5b
Branyoe <branyoe.dev@gmail.com>
2025-06-09 20:02:39 -0600

    feat(TourAccess): add expired field to track access expiration

--- src/controllers/accreditations.controllers.ts
+++ src/controllers/accreditations.controllers.ts
@@ -1,7 +1,12 @@
 import { RequestHandler } from "express";
 import { ResponseData } from "../types/ResponseData";
+import prisma from "../prisma";
 
 export const getAllAccreditationsController: RequestHandler = async (req, res) => {
+  prisma.tourAccess.findMany({
+    where: {
+      
+    }
   return res.status(200).json({
     ok: true,
     message: "Accreditations fetched successfully",
